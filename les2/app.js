dcf87658a1a8d7cca90374453cf039f10a1a35ba
Turbinist <srudyjju@gmail.com>
2018-07-03 06:37:13 +0300

    Add seconf task to les2

--- les2/app.js
+++ les2/app.js
@@ -1,35 +1,60 @@
-function powStandart(a,n) {
-  let res = a**n;
-  return res;  
-}
+// Возведение в степечь любого целого положительного числа
 
-function powLoop(a,n) {
-  if (n <= 0 || Number.isInteger(n)==false ) {
-    return NaN;
-  }else{
-    let res = 1;
-    for (let i = 1; i<=n; i++) {
-      res *= a;
-    }
-    return res;
+// function powStandart(a,n) {
+//   let res = a**n;
+//   return res;  
+// }
+
+// function powLoop(a,n) {
+//   if (n <= 0 || Number.isInteger(n)==false ) {
+//     return NaN;
+//   }else{
+//     let res = 1;
+//     for (let i = 1; i<=n; i++) {
+//       res *= a;
+//     }
+//     return res;
+//   }
+// }
+
+// function powRecursion(a,n) {
+//   if (n <= 0 || Number.isInteger(n)==false ) {
+//     return NaN;
+//   }else{
+//     let res = a 
+//     if (n == 1) {
+//       return res;
+//     }else{
+//       res = a * powRecursion(a,n - 1);    
+//       return res;
+//     }
+//   }
+// }
+
+// console.log(powStandart(2,3));
+// console.log(powLoop(2,2));
+// console.log(powRecursion(2,5));
+
+
+// Функция которая возвращвет сумму всех своих аргументов(числа)
+function sumNumbers() {
+  let sum = 0
+  for (let i in arguments) {
+    sum += arguments[i];
   }
+  return sum;  
 }
 
-function powRecursion(a,n) {
-  if (n <= 0 || Number.isInteger(n)==false ) {
-    return NaN;
-  }else{
-    let res = a 
-    if (n == 1) {
-      return res;
-    }else{
-      res = a * powRecursion(a,n - 1);    
-      return res;
-    }
+console.log(sumNumbers(1,2.6,-3));
+
+// Объединяет строки
+function sumStrings() {
+  let sum = '';
+  for (let i in arguments) {
+    sum += arguments[i];
   }
+  return sum;  
 }
 
-console.log(powStandart(2,3));
-console.log(powLoop(2,2));
-console.log(powRecursion(2,5));
+console.log(sumStrings('aaaaa','bbbbbbbb','cccccccc'));
 
