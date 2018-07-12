7f50a6987cdf4a22168542619f383cb394bf98a6
Turbinist <srudyjj@gmail.com>
2018-07-12 17:11:22 +0300

    task3 is done

---
+++ les5/animation/app.js
@@ -0,0 +1,40 @@
+const ball = document.querySelector('img');
+let opacity = 0;
+let topPx = 0;
+let left = 0;
+let time = 0;
+
+const x = setInterval(() => {
+  opacity = opacity + 0.01;
+  ball.style.opacity = opacity;
+  
+  if (opacity >= 1) {
+    clearInterval(x);
+    ball.style.opacity = 1;
+  }
+}, 50);
+
+const y = setInterval(() => {
+  topPx = topPx + 10;
+  ball.style.top = topPx + "px";
+}, 50);
+
+setInterval(() => {
+  topPx = -0;
+}, 2000);
+
+
+const t = setInterval(() => {
+  time ++;
+  console.log(time);
+  if (time >= 30) {
+    clearInterval(y);
+    clearInterval(t);
+  }
+  
+}, 1000);
+
+
+
+
+
